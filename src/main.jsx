ce976f4016383999b93e59972d303f60cdc53497
Pavel Shakuro <sh.pash.v@gmail.com>
2025-03-09 23:50:10 +0300

    hw2

---
+++ src/main.jsx
@@ -0,0 +1,44 @@
+import {createRoot} from 'react-dom/client';
+import {restaurants} from './mock';
+
+console.log(restaurants);
+const root = document.getElementById('root');
+
+const reactRoot = createRoot(root);
+
+reactRoot.render(
+    <div className='container'>
+        {restaurants.map(item => (
+            <div key={item.id} className="restaurant-card">
+                <h2 style={{textAlign: 'center'}}>{item.name}</h2>
+                <h3>Меню</h3>
+                <ul className="menu">
+                    {item.menu.map(dish => (
+                        <li key={dish.id}>
+                            <p>
+                            {dish.name} - ${dish.price}
+                                </p>
+                            <i>
+                                {dish.ingredients.join(', ')}
+                            </i>
+                        </li>
+                    ))}
+                </ul>
+                <h3>Отзывы</h3>
+                <ul className="reviews-list">
+                    {item.reviews.map(review => (
+                        <li key={review.id} className="review">
+                            <h5 className="review-user">
+                                {review.user}
+                            </h5>
+                            <i>
+                                {review.text}
+                            </i>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        ))
+        }
+    </div>
+)
